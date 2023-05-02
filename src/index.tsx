565409571ffc87650d1862866a3f76465ee03e33
GhostWheat <32331476+GhostWheat@users.noreply.github.com>
2023-05-02 08:32:22 -0500

    reinstalling

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles.css';
+import {Home} from './components';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Home title={'Cars Inv'}/>
   </React.StrictMode>
 );
 
