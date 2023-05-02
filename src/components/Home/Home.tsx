f0474c2b0e73e597d21964e323fc87f01fe451c6
GhostWheat <32331476+GhostWheat@users.noreply.github.com>
2023-05-02 09:22:24 -0500

    building out front page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -8,11 +8,11 @@ interface Props{
     title:string;
 }
 
-export const Home = ( props:Props ) => {
-    return (
-        <h1> Hi sup fools</h1>
-    )
-}
+// export const Home = ( props:Props ) => {
+//     return (
+//         <h1> Hi sup fools</h1>
+//     )
+// }
 
 // Create Styled Components with styled-components
 
@@ -23,14 +23,63 @@ const Root = styled('div')({
 
 const NavbarContainer = styled('div')({
     display: 'flex',
-    justifyContent: 'space-evenly',
-    alignItems: 'center'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'lightseagreen',
 })
 
-const HomeLogo = styled('div')({
-    marginLeft: '0.45em',
+const Logo = styled('div')({
+    marginRight: '0.5em',
+    marginLeft: '0.5em',
 })
 
-const HomeAnchor = styled('a')({
-    
+const LogoA = styled('a')({
+    color: 'black',
+    listStyle: 'none',
+    textTransform: 'uppercase',
+    textDecoration: 'underline dotted'
 })
+
+const NavList = styled('ul')({
+    listStyle: 'none',
+    textTransform: 'uppercase',
+    display: 'flex',
+})
+
+const NavA = styled('a')({
+    display: 'block',
+    padding: '1em',
+    color: 'lightgrey',
+    textDecoration: 'none',
+})
+
+const Main = styled('main')({
+    backgroundImage: `linear gradient(green,green,url(${splashImage}))`
+})
+
+export const Home = (props:Props) => {
+    return(
+        <Root>
+            <NavbarContainer>
+                <NavList>
+                    <NavA href='#'>Home</NavA>
+                    <NavA href='#'>About</NavA>
+                    <NavA href='#'>My Dashboard</NavA>
+                    <NavA href='#'>#</NavA>
+                </NavList>
+
+                <Logo>
+                    <LogoA href='#'>COMPANYBRANDWEBSITE</LogoA>
+                </Logo>
+
+
+
+            </NavbarContainer>
+
+
+
+
+
+        </Root>
+    )
+}
