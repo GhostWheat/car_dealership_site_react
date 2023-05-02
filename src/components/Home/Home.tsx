dc4bf498643a5a12a962dfe936736ed81cbd1c3e
GhostWheat <32331476+GhostWheat@users.noreply.github.com>
2023-05-02 08:48:48 -0500

    building out basic homepage struct w/Styled

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import {styled} from '@mui/system';
+import {Button} from '@mui/material';
+import splashImage from '../../assets/images/cash-macanaya-_5lTTNE2L5I-unsplash.jpg'
+
 
 interface Props{
     title:string;
@@ -9,3 +13,24 @@ export const Home = ( props:Props ) => {
         <h1> Hi sup fools</h1>
     )
 }
+
+// Create Styled Components with styled-components
+
+const Root = styled('div')({
+    padding:0,
+    margin:0,
+})
+
+const NavbarContainer = styled('div')({
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center'
+})
+
+const HomeLogo = styled('div')({
+    marginLeft: '0.45em',
+})
+
+const HomeAnchor = styled('a')({
+    
+})
