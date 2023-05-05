356c87e29b564a9a9ce8c5abee3b98817603eae9
GhostWheat <32331476+GhostWheat@users.noreply.github.com>
2023-05-04 21:38:27 -0500

    frontpage v1 is good to go

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,7 +3,9 @@ import { styled } from '@mui/system';
 import { Button } from '@mui/material';
 import unsplashImage from '../../assets/images/cash-macanaya-_5lTTNE2L5I-unsplash.jpg'
 import { Link } from 'react-router-dom';
-import { MarginRounded } from '@mui/icons-material';
+import { BorderAll, MarginRounded } from '@mui/icons-material';
+import ScaledText from 'react-scale-text';
+
 
 interface Props {
     title: string;
@@ -20,7 +22,9 @@ interface Props {
 const Root = styled('div')({
     padding: 0,
     margin: 0,
-    backgroundImage: `linear-gradient(to bottom, rgba(10,133,203, 0.5) 5.5%, rgba(204,255,255, 0.5) 6%, rgba(204,255,255, 0.5) 6.3%, rgba(204,255,255, 0) 6.5%, rgba(0,0,0,0) 0%, rgba(0,0,0, .3) ), url(${unsplashImage})`,
+    backgroundImage: `linear-gradient(to bottom, rgba(10,133,203, 0.5),  rgba(0,0,0,0) 0%, rgba(0,0,0, .3) ), url(${unsplashImage})`,
+    // backgroundImage: `linear-gradient(to bottom, rgba(10,133,203, 0.5) 5.5%, rgba(204,255,255, 0.5) 6%, rgba(204,255,255, 0.5) 6.3%, rgba(204,255,255, 0) 6.5%, rgba(0,0,0,0) 0%, rgba(0,0,0, .3) ), url(${unsplashImage})`,
+    // backgroundImage: `linear-gradient(to bottom, rgba(204,255,255, 0.8) 2%, rgba(204,255,255, 0.8) 2%, rgba(204,255,255, 0.75) 2%, rgba(204,255,255, 0.75) 2%, rgba(0,0,0,0) 40%, rgba(0,0,0, .4)), url(${unsplashImage})`,
     width: '100%',
     height: '100%',
     backgroundSize: 'cover',
@@ -31,6 +35,9 @@ const Root = styled('div')({
 })
 
 const NavbarContainer = styled('div')({
+    minHeight: '50px',
+    backgroundColor: 'rgba(10,133,203, 0.5)',
+    borderBottom: '3px rgba(204,255,255, 0.4) groove',
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
@@ -44,22 +51,39 @@ const NavList = styled('ul')({
 })
 
 const NavA = styled(Link)({
-    display: 'block',
-    padding: '1em',
-    color: 'lightgrey',
+    display: 'flex',
+    padding: '0.5em',
+    color: 'white',
     textDecoration: 'none',
+    textAlign: 'center',
+    fontSize: 12,
+    // justifyItems: 'end',
+    // alignContent: 'baseline',
+    alignItems: 'center',
+    justifyContent: 'center',
+    // border: '1px solid black'
+    // maxHeight: '10px'
 })
 
 const Logo = styled('div')({
+    height: '60px',
+    width: '60px',
+    display:'flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
     marginRight: '0.5em',
     marginLeft: '0.5em',
+    border: 'solid white 1px',
+    borderRadius: '100%'
 })
 
 const LogoA = styled('a')({
-    color: 'black',
+    color: 'white',
     listStyle: 'none',
+    fontSizeAdjust: 'inherit',
     textTransform: 'uppercase',
-    textDecoration: 'underline dotted'
+    textDecoration: 'none'
 })
 
 const AcctDiv = styled('div')({
@@ -67,19 +91,24 @@ const AcctDiv = styled('div')({
     marginLeft: '0.5em',
 })
 
-const AcctDropdown = styled('h1')({
+const AcctLink = styled(Link)({
     // backgroundColor: 'cyan',
+    color: 'white',
+    fontSize: 12,
+    textTransform: 'uppercase',
+    textDecoration: 'none'
 })
 
 const Main = styled('main')({
-
+    // backgroundImage: `linear-gradient(to bottom, rgba(10,133,203, 0.5) 5.5%, rgba(204,255,255, 0.5) 6%, rgba(204,255,255, 0.5) 6.3%, rgba(204,255,255, 0) 6.5%, rgba(0,0,0,0) 0%, rgba(0,0,0, .3) ), url(${unsplashImage})`,
     // backgroundImage: `linear-gradient(to bottom, rgba(204,255,255, 0.8) 2%, rgba(204,255,255, 0.8) 2%, rgba(204,255,255, 0.75) 2%, rgba(204,255,255, 0.75) 2%, rgba(0,0,0,0) 40%, rgba(0,0,0, .4)), url(${unsplashImage})`,
+    borderTop: '3px rgba(204,255,255, 0.4) ridge',
     width: '100%',
     height: '100%',
-    // backgroundSize: 'cover',
-    // backgroundRepeat: 'no-repeat',
-    // backgroundPosition: 'center',
-    // position: 'absolute',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    position: 'absolute',
     // backgroundColor: 'linen',
 })
 
@@ -99,19 +128,18 @@ export const Home = (props: Props) => {
             <NavbarContainer>
                 <NavList>
                     <NavA to='/'>Home</NavA>
-                    <NavA to='About'>About</NavA>
+
                     <NavA to='/dashboard'>My Dashboard</NavA>
-                    <NavA to='/signin'>#</NavA>
+                    <NavA to='#'>About</NavA>
+                    <NavA to='/signin'>Sign In</NavA>
                 </NavList>
 
                 <Logo>
-                    <LogoA href='/'>COMPANYBRANDWEBSITE</LogoA>
+                    <LogoA href='/'>BRAND</LogoA>
                 </Logo>
 
                 <AcctDiv>
-                    <AcctDropdown>
-                        My Account
-                    </AcctDropdown>
+                    <AcctLink to='/account'>My Account</AcctLink>
                 </AcctDiv>
 
             </NavbarContainer>
@@ -120,7 +148,7 @@ export const Home = (props: Props) => {
                 <MainText id="maintext">
                     <h1>{props.title}</h1>
                     <p>CAR IS GOOD</p>
-                    <Button sx={{marginTop: '10px',boxShadow: '1px 0 0 #000, 0 -1px 0 #000, 0 1px 0 #000, -1px 0 0 #000'}} color='inherit' variant='outlined'><i>GO TO CARS</i></Button>
+                    <Button sx={{ marginTop: '10px', boxShadow: '1px 0 0 #000, 0 -1px 0 #000, 0 1px 0 #000, -1px 0 0 #000' }} color='inherit' variant='outlined'><i>GO TO CARS</i></Button>
                 </MainText>
             </Main>
 
---
+++ src/components/Home/react-scale-text.d.ts
@@ -0,0 +1,9 @@
+declare module 'react-scale-text' {
+    // interface ScaledText {
+    //     minFontSize: number;
+    //     maxFontSize: number;
+    //     widthonly: boolean;
+    // }
+
+export default ScaledText
+}
